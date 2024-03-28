0a630056e4f7f4e11c35900323b49025cf0321b5
John Pontes <jpontes@questrade.com>
2024-03-27 23:40:42 -0300

    feat: Adiciona o componente Loading

--- react-native/ignite/igniteteams/App.tsx
+++ react-native/ignite/igniteteams/App.tsx
@@ -1,11 +1,16 @@
-import theme from "@theme/index";
-import { Groups } from "src/Screens/Groups";
-import { ThemeProvider } from "styled-components";
+import theme from '@theme/index';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import { Groups } from 'src/Screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
