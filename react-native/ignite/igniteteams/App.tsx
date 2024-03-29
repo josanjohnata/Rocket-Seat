473b2657efaaff00f79659b4f7cd6fdbcc774c83
John Pontes <jpontes@questrade.com>
2024-03-28 23:46:05 -0300

    feat: Adiciona o componente Header

--- react-native/ignite/igniteteams/App.tsx
+++ react-native/ignite/igniteteams/App.tsx
@@ -4,12 +4,18 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/r
 import { Groups } from 'src/Screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
