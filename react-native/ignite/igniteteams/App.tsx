ffdc213c7cb0a90465ed6a60b7d582f732cdb8fb
John Pontes <jpontes@questrade.com>
2024-04-21 23:57:48 -0300

    feat: Adiciona o componente Filter e a listagem de filtros

--- react-native/ignite/igniteteams/App.tsx
+++ react-native/ignite/igniteteams/App.tsx
@@ -1,12 +1,12 @@
 import theme from '@theme/index';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
-import { Groups } from 'src/Screens/Groups';
+import { Groups } from './src/Screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
 import { StatusBar } from 'react-native';
-import { NewGroup } from 'src/Screens/NewGroup';
-import { Players } from 'src/Screens/Players';
+import { NewGroup } from './src/Screens/NewGroup';
+import { Players } from './src/Screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
@@ -22,4 +22,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-
--- react-native/ignite/igniteteams/src/Screens/Players/index.tsx
+++ react-native/ignite/igniteteams/src/Screens/Players/index.tsx
@@ -1,20 +1,52 @@
-import { Header } from '@components/Header';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
 
-import { Container } from './styles';
+import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
+
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
 
       <Highlight
         title='Nome da turma'
-        subtitle='adicione a galera e separes os times'
+        subtitle='adicione a galera e separe os times'
       />
 
-      <ButtonIcon />
+      <Form>
+        <Input
+          placeholder='Nome da pessoa'
+          autoCorrect={false}
+        />
+
+        <ButtonIcon icon='add' />
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 };
